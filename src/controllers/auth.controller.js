586ab90053342c650403d1a1a323813ae8e448df
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-10-09 10:22:56 -0500

    [+] Token JWT /auth/signup: Registrar Usuarios

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,11 @@
-import User from '../models/User';
-// Controller Auth
+import jwt from 'jsonwebtoken';
 
+import app from '../config/app';
+
+// Models
+import User from '../models/User';
+
+// Controller Auth
 export const signIn = ( request, response ) => {
     response .json( 'singIn' );
 }
@@ -20,10 +25,19 @@ export const signUp = async ( request, response ) => {
     const savedUser = await newUser .save();
     console.log( savedUser );
 
+    /** Crea el Token usando JWT */
+    const token = jwt .sign(
+        { id: savedUser._id },
+        app.SECRET,
+        { expiresIn: 86400 /** 24h */ }
+    );
+    console.log( token );
+
     response .json({      // status: 204
         method: 'POST',
         path: `/api/auth/signup`,
         msg: 'Sign Up',
-        newUser
+        newUser,
+        token
     });
 }
